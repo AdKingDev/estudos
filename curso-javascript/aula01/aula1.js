27a197ff3d3c242966d503f28a4ea41a386fae6c
Adriano Rodrigues <a.rodrigues.sobrinho4@gmail.com>
2026-01-30 17:32:25 -0300

    curso de javascript

---
+++ curso-javascript/aula01/aula1.js
@@ -0,0 +1,1 @@
+console.log("Olá, mundo! Este é meu primeiro script em JavaScript.");
---
+++ curso-javascript/aula02/aula2.js
@@ -0,0 +1,38 @@
+/* Variáveis em JavaScript
+
+    const (Não pode ser reatribuída)
+    let (Pode ser reatribuída)
+    var (NÃO é recomendado USAR)
+
+*/
+
+const nome = 'King'
+let idade = 25
+
+
+/* Tipos de Dados em JavaScript
+
+    String -> Texto
+    Number -> Números
+    Boolean -> true ou false
+    Array -> Vetores ou Listas
+    Object -> Objetos
+    Null -> Nulo
+    Undefined -> Indefinido
+
+
+*/
+
+const texto = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.` // String
+const numero = 10 // Number
+const booleano = true // Boolean
+const lista = ['Maçã', 'Banana', 'Laranja'] // Array
+const pessoa = {
+    nome: 'King',
+    idade: 27,
+    conjuge: null, // Null
+    profissao: undefined // Undefined
+} // Object
+
+console.log(pessoa.nome) // Acessando o nome do objeto pessoa
+console.log(lista[1]) // Acessando o segundo item da lista
